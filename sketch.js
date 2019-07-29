fbbce0f857d1f36c9563e722da1c6641c7468aa8
candrewlee14 <32912555+candrewlee14@users.noreply.github.com>
2019-07-29 00:03:35 -0600

    added new batch of changes involving feeding children and exporting data

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 let fr = 60;
 let bug; // Declare object
 let foodAmount = 10;
@@ -6,24 +7,84 @@ let bugAmount = 10;
 let bugArray = [];
 let drawLine = true;
 let scribble = new Scribble();
-let mutationFactor = .2;
+let defaultMutationFactor = .2;
 let foodRespawnRate = 60;
-let foodNumForBaby = 2;
-let lifespan = 10 * fr;
+let foodNumForBaby = 3;
+let lifespan = 25 * fr;
 let foodLifespanFactor = 120;
 let blindTimer = 200;
+let defaultGiveFoodToKidOnBirthProb = 1;
+let mutateMutationFactor = true;
+let totalBugs = 0;
+let simWidth;
+let simHeight;
 
+let globalsTable = new p5.Table();
+
+
+
+let bugTable = new p5.Table();
+
+
+
+function boolFromProb(p){
+  if (random(0,1) <= p)
+    return true;
+  else
+    return false;
+}
 
 function setup() {
   frameRate(fr);
   noStroke();
-  createCanvas(window.windowWidth, window.windowHeight);
+
+globalsTable.addColumn('startingFoodAmount');
+globalsTable.addColumn('startingBugAmount');
+globalsTable.addColumn('framerate');
+globalsTable.addColumn('lifespan');
+globalsTable.addColumn('foodRespawnRate');
+globalsTable.addColumn('foodNumForBaby');
+globalsTable.addColumn('defaultGiveFoodToKidOnBirthProb');
+globalsTable.addColumn('mutationFactor');
+globalsTable.addColumn('mutateMutationFactor');
+globalsTable.addColumn('blindTimer');
+globalsTable.addColumn('simWidth');
+globalsTable.addColumn('simHeight');
+let newGRow = globalsTable.addRow();
+newGRow.setNum('startingFoodAmount',foodAmount);
+newGRow.setNum('startingBugAmount',bugAmount);
+newGRow.setNum('framerate',fr);
+newGRow.setNum('lifespan',lifespan);
+newGRow.setNum('foodRespawnRate',foodRespawnRate);
+newGRow.setNum('foodNumForBaby',foodNumForBaby);
+newGRow.setNum('defaultGiveFoodToKidOnBirthProb',defaultGiveFoodToKidOnBirthProb);
+newGRow.setNum('mutateMutationFactor',mutateMutationFactor);
+newGRow.setNum('blindTimer',blindTimer);
+newGRow.setNum('simWidth',simWidth);
+newGRow.setNum('simHeight',simHeight);
+
+bugTable.addColumn('id');
+bugTable.addColumn('sightDiameter');
+bugTable.addColumn('smellDiameter');
+bugTable.addColumn('maxAngleChange');
+bugTable.addColumn('diameter');
+bugTable.addColumn('speed');
+bugTable.addColumn('giveFoodToKidOnBirthProb');
+bugTable.addColumn('mutationFactor');
+bugTable.addColumn('parentId');
+bugTable.addColumn('birthFrame');
+bugTable.addColumn('deathFrame');
+bugTable.addColumn('foodEaten');
+bugTable.addColumn('childrenCount');
+  simWidth = window.windowWidth;
+  simHeight = window.windowHeight;
+  createCanvas(simWidth, simHeight);
   // Create object
   for (let i = 0; i < foodAmount; i++){
       foodArray.push(new Food(i));
     }
   for (let i = 0; i < bugAmount; i++){
-      bugArray.push(new Jitter(i));
+      bugArray.push(new Jitter(i, i));
     }
   console.log(foodArray.length);
 }
@@ -31,8 +92,8 @@ function setup() {
 class Food{
  constructor(i) {
    this.index = i; 
-    this.x = random(window.windowWidth/2-window.windowWidth/3,window.windowWidth/2+window.windowWidth/3);
-   this.y = random(window.windowHeight/2-window.windowHeight/3,window.windowHeight/2+window.windowHeight/3); 
+    this.x = random(simWidth/2-simWidth/3,simWidth/2+simWidth/3);
+   this.y = random(simHeight/2-simHeight/3,simHeight/2+simHeight/3); 
    this.size = random(10,20);
    this.color = [random(100,255),random(100,255),random(100,255),];
    this.timer = 0;
@@ -50,11 +111,16 @@ class Food{
 
 function draw() {
   background(50, 89, 100);
-  for (let i = 0; i < bugArray.length; i++){
+  for (let i = 0; i < bugAmount; i++){
     bugArray[i].display();
     bugArray[i].smellAndSeeAndTouch();
     bugArray[i].move();
     
+  }
+  if (bugAmount==0){
+    save(globalsTable,'globalsTable.csv');
+    save(bugTable,'bugTable.csv');
+    noLoop();
   }
   for (let i = 0; i < foodArray.length; i++){
     foodArray[i].display();
@@ -62,31 +128,54 @@ function draw() {
   if (frameCount%foodRespawnRate==0){
     foodAmount++;
     foodArray.push(new Food(foodArray.length-1));
+    for (let i = 0; i < foodAmount; i++){
+      foodArray[i].index = i; 
+    }
   }
+
 }
 
 function compareNumbers(a, b) {
   return a.distance - b.distance;
 }
 
-function mutate(bug){
-  bug.SightDiameter += random(-bug.SightDiameter*mutationFactor, bug.SightDiameter*mutationFactor);
-  bug.SmellDiameter += random(-bug.SmellDiameter*mutationFactor, bug.SmellDiameter*mutationFactor);
-  bug.maxAngleChange += random(-bug.maxAngleChange*mutationFactor, bug.maxAngleChange*mutationFactor);
-  bug.diameter += random(-bug.diameter*mutationFactor, bug.diameter*mutationFactor);
-  bug.speed += random(-bug.speed*mutationFactor, bug.speed*mutationFactor);
-  return bug;
+function addBugToTable(bug,mutated){
+  let newRow;
+  if (!mutated)
+    newRow = bugTable.addRow();
+  else
+    newRow = bugTable.getRow(bug.id-1);
+  newRow.setNum('id',bug.id);
+  newRow.setNum('sightDiameter',bug.SightDiameter);
+  newRow.setNum('smellDiameter',bug.SmellDiameter);
+  newRow.setNum('maxAngleChange',bug.maxAngleChange);
+  newRow.setNum('diameter',bug.diameter);
+  newRow.setNum('speed',bug.speed);
+  newRow.setNum('giveFoodToKidOnBirthProb',bug.giveFoodToKidOnBirthProb);
+  newRow.setNum('mutationFactor',bug.mutationFactor);
+  newRow.setNum('parentId',bug.parentId);
+  newRow.setNum('birthFrame',frameCount);
+  newRow.setString('deathFrame','living');
+  newRow.setNum('foodEaten', bug.foodParticlesEaten);
+  newRow.setNum('childrenCount',bug.childrenCount);
 }
 
 // Jitter class
 class Jitter {
   constructor(i) {
+    this.parentId=0;
+    this.id = totalBugs+1;
+    totalBugs++;
+    this.childrenCount = 0;
+    this.mutationFactor = defaultMutationFactor;
+    this.giveFoodToKidOnBirthProb = defaultGiveFoodToKidOnBirthProb;
     this.index = i;
     this.lifespan = lifespan;
     this.SightDiameter = random(40,90);
     this.SmellDiameter = random(100,200);
-    this.x = random(0,window.windowWidth);
-    this.y = random(0,window.windowHeight);
+    this.foodParticlesEaten = 0;
+    this.x = random(0,simWidth);
+    this.y = random(0,simHeight);
     this.angle = random(0, 2 * PI);
     this.maxAngleChange = 0.3;
     this.distance = 0;
@@ -104,14 +193,36 @@ class Jitter {
     this.hatDiameter = this.diameter*2/3;
     this.hatColor = color(0,0,0);
     this.alive = true;
+    //table stuff
+    addBugToTable(this, false);
+  }
+
+  mutate(bug){
+    bug.SightDiameter += random(-bug.SightDiameter*this.mutationFactor, bug.SightDiameter*this.mutationFactor);
+    bug.SmellDiameter += random(-bug.SmellDiameter*this.mutationFactor, bug.SmellDiameter*this.mutationFactor);
+    bug.maxAngleChange += random(-bug.maxAngleChange*this.mutationFactor, bug.maxAngleChange*this.mutationFactor);
+    bug.diameter += random(-bug.diameter*this.mutationFactor, bug.diameter*this.mutationFactor);
+    bug.speed += random(-bug.speed*this.mutationFactor, bug.speed*this.mutationFactor);
+    bug.giveFoodToKidOnBirthProb += random(-bug.giveFoodToKidOnBirthProb*this.mutationFactor, bug.giveFoodToKidOnBirthProb*this.mutationFactor)
+    if (bug.giveFoodToKidOnBirthProb <0)
+      bug.giveFoodToKidOnBirthProb = 0;
+      if (bug.giveFoodToKidOnBirthProb >1)
+      bug.giveFoodToKidOnBirthProb = 1;
+    if (mutateMutationFactor){
+      bug.mutationFactor += random(-bug.mutationFactor* this.mutationFactor, bug.mutationFactor* this.mutationFactor);}
+    addBugToTable(bug,true);
+    return bug;
   }
 
   reproduce(){
     if (this.foodStorage >= foodNumForBaby){
-      this.foodStorage = 0;
+      this.foodStorage -= foodNumForBaby;
+      this.childrenCount++;
+      let row = bugTable.getRow(this.id-1);
+      row.setNum('childrenCount',this.childrenCount);
       bugAmount++;
       bugArray.push(this.clone());
-      console.log(bugArray.length);
+      //console.log(bugArray.length);
     }
   }
 
@@ -132,6 +243,8 @@ class Jitter {
       for (let i = 0; i < bugAmount; i++){
         bugArray[i].index = i;
       }
+      let row = bugTable.getRow(this.id-1);
+      row.setNum('deathFrame',frameCount);
     }
   }
 
@@ -165,6 +278,9 @@ class Jitter {
   }
   clone() {
     let newBug = new Jitter();
+    newBug.parentId = this.id;
+    newBug.giveFoodToKidOnBirthProb = this.giveFoodToKidOnBirthProb;
+    newBug.mutationFactor = this.mutationFactor;
     newBug.SightDiameter = this.SightDiameter;
     newBug.SmellDiameter = this.SmellDiameter;
     newBug.x = this.x;
@@ -173,11 +289,14 @@ class Jitter {
     newBug.diameter = this.diameter;
     newBug.speed = this.speed
     newBug.hatDiameter = newBug.diameter*2/3;
-    return mutate(newBug);
+    if (boolFromProb(this.giveFoodToKidOnBirthProb)){
+      newBug.foodStorage = this.foodStorage;
+      this.foodStorage = 0;
+    }
+    return this.mutate(newBug);
   }
   energySpent(){
-    let energy = sqrt(sqrt(this.speed) + sqrt((this.SightDiameter+this.SmellDiameter)/2))/2; 
-    text(energy,100,100);
+    let energy = sqrt(sqrt(this.speed) + sqrt((this.SightDiameter+this.SmellDiameter)/2) + sqrt(this.diameter)/2)/2; 
     return energy;
   }
   move() {
@@ -197,17 +316,17 @@ class Jitter {
       this.angle = 0;
       this.x = 0;
     }
-    if (this.x > window.windowWidth){
+    if (this.x > simWidth){
       this.angle = PI;
-      this.x = window.windowWidth;
+      this.x = simWidth;
     }
     if (this.y < 0){
       this.angle = 1/2 * PI;
       this.y = 0;
     }
-    if (this.y > window.windowHeight){
+    if (this.y > simHeight){
       this.angle = 3/2 * PI;
-      this.y = window.windowHeight;
+      this.y = simHeight;
     }
     
   }
@@ -231,17 +350,27 @@ class Jitter {
             //if eaten
             this.color = color(0,0,0);   
             this.foodStorage+=this.foodDistArray[0].obj.nutritionalValue;  
+            this.foodParticlesEaten++;
             this.lifespan+=this.foodDistArray[0].obj.nutritionalValue * foodLifespanFactor;          
             foodAmount--;
             foodArray.splice(this.foodDistArray[0].obj.index,1);
+            console.log(foodAmount);
             this.foodDistArray.splice(0,1);
             for (let i = 0; i < foodAmount; i++){
               foodArray[i].index = i; 
             }
+            let row = bugTable.getRow(this.id-1);
+            row.setNum('foodEaten',this.foodParticlesEaten);
           }
           else {
             //if seen but not eaten
             this.angle = this.getAngle(this.foodDistArray[0].obj);
+            if (sqrt(sq(this.x-this.foodDistArray[0].obj.x)+sq(this.y-this.foodDistArray[0].obj.y))<this.speed)
+            {
+              this.x = this.foodDistArray[0].obj.x;
+              this.y = this.foodDistArray[0].obj.y;
+            }
+
             this.color = color(0,70,150)
           }
         }
@@ -259,7 +388,7 @@ class Jitter {
         this.naturalColor = this.getBodyColorByTraits();
         this.color = this.naturalColor;      
         if (this.timer%blindTimer == 0){
-          this.angle = this.getAngle(this.foodDistArray[0].obj);
+          this.angle = this.getAngle(this.foodDistArray[0].obj) + random(-HALF_PI/2,HALF_PI/2);
         }
       }
     }
